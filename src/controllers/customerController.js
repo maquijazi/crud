0b1e29fb01ea2bca6964cac48c76023f9d03e6c0
maquijazi <xavi90_8@hotmail.com>
2021-05-31 10:03:19 +0200

    sin var int

--- src/controllers/customerController.js
+++ src/controllers/customerController.js
@@ -1,5 +1,5 @@
 const controller = {};//Objeto
-var int = 7;
+
 controller.list = (req, res) => { //Función que manda el hello world
    req.getConnection((err, conn) => {
        conn.query('SELECT * FROM customer', (err, customers) =>{
